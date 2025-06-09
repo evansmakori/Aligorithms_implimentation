c9b607f74b2c257241881d66998640e5c9c81098
evansmakori <evamakorieva@gmail.com>
2025-06-09 20:18:03 +0200

    Added data structures folder containing all workings related to data structures

---
+++ data_structures/linked_lists.py
@@ -0,0 +1,54 @@
+class Node:
+    # An object for storing a single node of a linked list
+    def __init__(self, data):
+        self.data = data
+        self.next_node = None
+
+    def __repr__(self):
+        return "<Node data: %s>" % self.data
+
+
+class LinkedList:
+    # Singly linked list
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head is None
+
+    def size(self):
+        # Returns the number of nodes in the list taking O(n) time
+        current = self.head
+        count = 0
+
+        while current:
+            count += 1
+            current = current.next_node
+
+        return count
+
+    def add(self, data):
+        # Add new node containing data at the head of the list
+        new_node = Node(data)
+        new_node.next_node = self.head
+        self.head = new_node
+
+    def __repr__(self):
+        """
+        Return a string representation of the list
+        Takes O(n) time
+        """
+        nodes = []
+        current = self.head
+
+        while current:
+            if current is self.head:
+                nodes.append("[Head: %s]" % current.data)
+            elif current.next_node is None:
+                nodes.append("[Tail: %s]" % current.data)
+            else:
+                nodes.append("[%s]" % current.data)
+
+            current = current.next_node
+
+        return ' -> '.join(nodes)
